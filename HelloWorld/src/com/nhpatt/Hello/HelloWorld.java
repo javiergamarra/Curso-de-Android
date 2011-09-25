430fef02f529e21cdab436e252625a34549a2072
Javier Gamarra <nhpatt@gmail.com>
2011-09-26 00:12:18 +0200

    Ejercicio 8: Adapters

--- HelloWorld/src/com/nhpatt/Hello/HelloWorld.java
+++ HelloWorld/src/com/nhpatt/Hello/HelloWorld.java
@@ -3,16 +3,18 @@ package com.nhpatt.Hello;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class HelloWorld extends Activity implements OnClickListener {
+public class HelloWorld extends ListActivity implements OnClickListener {
 
 	private final List<String> notas = new ArrayList<String>();
+	private ArrayAdapter<String> adapter;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -24,6 +26,9 @@ public class HelloWorld extends Activity implements OnClickListener {
 		final Button salir = (Button) findViewById(R.id.salir);
 		salir.setOnClickListener(this);
 		System.out.println("Creating activity...");
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, notas);
+		setListAdapter(adapter);
 	}
 
 	public void onClick(final View v) {
@@ -31,6 +36,8 @@ public class HelloWorld extends Activity implements OnClickListener {
 		case R.id.enlace:
 			final TextView text = (TextView) findViewById(R.id.text);
 			notas.add(text.getText().toString());
+			text.setText("");
+			adapter.notifyDataSetChanged();
 			break;
 		case R.id.salir:
 			finish();
