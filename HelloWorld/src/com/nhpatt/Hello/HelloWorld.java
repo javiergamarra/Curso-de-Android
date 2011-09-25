ab577e4e7299cf24a1a4050b780dcd0e165c2343
Javier Gamarra <nhpatt@gmail.com>
2011-09-25 18:29:10 +0200

    Exercise 1, logging state changes

--- HelloWorld/src/com/nhpatt/Hello/HelloWorld.java
+++ HelloWorld/src/com/nhpatt/Hello/HelloWorld.java
@@ -4,10 +4,36 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class HelloWorld extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		System.out.println("Creating activity...");
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		System.out.println("Starting an activity...");
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		System.out.println("Resuming an activity...");
+	}
+
+	@Override
+	public void onPause() {
+		super.onStop();
+		System.out.println("Pausing an activity...");
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		System.out.println("Stoping an activity...");
+	}
+
 }
