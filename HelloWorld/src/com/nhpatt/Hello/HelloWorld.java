7fdf0c8d78b00739d226dc3478fcc513a43d1f57
Javier Gamarra <nhpatt@gmail.com>
2011-09-25 21:02:21 +0200

    Ejercicio 5: boton funcionando

--- HelloWorld/src/com/nhpatt/Hello/HelloWorld.java
+++ HelloWorld/src/com/nhpatt/Hello/HelloWorld.java
@@ -2,13 +2,19 @@ package com.nhpatt.Hello;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class HelloWorld extends Activity {
+public class HelloWorld extends Activity implements OnClickListener {
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		final Button button = (Button) findViewById(R.id.enlace);
+		button.setOnClickListener(this);
 		System.out.println("Creating activity...");
 	}
 
@@ -34,6 +40,11 @@ public class HelloWorld extends Activity {
 	public void onStop() {
 		super.onStop();
 		System.out.println("Stoping an activity...");
+	}
+
+	public void onClick(final View v) {
+		final TextView text = (TextView) findViewById(R.id.text);
+		text.setText("Otra cosa");
 	}
 
 }
