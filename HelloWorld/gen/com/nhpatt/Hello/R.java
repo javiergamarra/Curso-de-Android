64898eede80fbae4acf5fb58ecdeca032b03b5cf
unknown <Administrador@.cexc.es>
2011-09-26 17:43:15 +0200

    Refactoring...

--- HelloWorld/gen/com/nhpatt/Hello/R.java
+++ HelloWorld/gen/com/nhpatt/Hello/R.java
@@ -14,16 +14,16 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int enlace=0x7f050001;
+        public static final int incluirNota=0x7f050001;
         public static final int salir=0x7f050002;
-        public static final int text=0x7f050000;
+        public static final int textoNota=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int enlace=0x7f040002;
+        public static final int incluirNota=0x7f040002;
         public static final int nota=0x7f040001;
         public static final int salir=0x7f040003;
     }
--- HelloWorld/src/com/nhpatt/Hello/HelloWorld.java
+++ HelloWorld/src/com/nhpatt/Hello/HelloWorld.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
@@ -13,28 +14,29 @@ import android.widget.TextView;
 
 public class HelloWorld extends ListActivity implements OnClickListener {
 
+	private static final String APPLICATION_TAG = "nhpattAPP";
 	private final List<String> notas = new ArrayList<String>();
 	private ArrayAdapter<String> adapter;
 
-	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		final Button button = (Button) findViewById(R.id.enlace);
+		final Button button = (Button) findViewById(R.id.incluirNota);
 		button.setOnClickListener(this);
 		final Button salir = (Button) findViewById(R.id.salir);
 		salir.setOnClickListener(this);
-		System.out.println("Creating activity...");
+		Log.d(APPLICATION_TAG, "Creating activity...");
 		adapter = new ArrayAdapter<String>(this,
 				android.R.layout.simple_list_item_1, notas);
 		setListAdapter(adapter);
 	}
 
+	@Override
 	public void onClick(final View v) {
 		switch (v.getId()) {
-		case R.id.enlace:
-			final TextView text = (TextView) findViewById(R.id.text);
+		case R.id.incluirNota:
+			final TextView text = (TextView) findViewById(R.id.textoNota);
 			notas.add(text.getText().toString());
 			text.setText("");
 			adapter.notifyDataSetChanged();
