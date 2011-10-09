becfa95b13a40710debd2e167bb1daa19b4b8510
Javier Gamarra <nhpatt@gmail.com>
2011-10-09 13:44:32 +0200

    Ejercicio 6: refinar interfaz con estilos

--- Notas/gen/com/nhpatt/notas/R.java
+++ Notas/gen/com/nhpatt/notas/R.java
@@ -10,20 +10,24 @@ package com.nhpatt.notas;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int green=0x7f040000;
+    }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int insertarNota=0x7f050000;
-        public static final int salir=0x7f050001;
+        public static final int insertarNota=0x7f060000;
+        public static final int salir=0x7f060001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int insertarNota=0x7f040001;
-        public static final int notaInsertada=0x7f040002;
-        public static final int salir=0x7f040003;
+        public static final int app_name=0x7f050000;
+        public static final int insertarNota=0x7f050001;
+        public static final int notaInsertada=0x7f050003;
+        public static final int nuevaNota=0x7f050002;
+        public static final int salir=0x7f050004;
     }
 }
