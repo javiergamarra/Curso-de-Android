5b4f6243d3abafa51dda272193eb8be5e80ccd9c
Javier Gamarra <nhpatt@gmail.com>
2011-10-09 14:03:44 +0200

    Ejercicio 8: recuperar un valor de un EditText

--- Notas/gen/com/nhpatt/notas/R.java
+++ Notas/gen/com/nhpatt/notas/R.java
@@ -17,8 +17,8 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int insertarNota=0x7f060000;
-        public static final int salir=0x7f060001;
+        public static final int incluirNuevaNota=0x7f060001;
+        public static final int nuevaNota=0x7f060000;
         public static final int textoInsertarNota=0x7f060002;
     }
     public static final class layout {
--- Notas/src/com/nhpatt/notas/NotasActivity.java
+++ Notas/src/com/nhpatt/notas/NotasActivity.java
@@ -1,24 +1,34 @@
 package com.nhpatt.notas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NotasActivity extends Activity implements OnClickListener {
+
+	private static final String NOTAS_TAG = "NOTAS";
+	private final List<String> notas = new ArrayList<String>();
 
 	@Override
 	public void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.nuevanota);
-		//
-		// final Button button = (Button) findViewById(R.id.salir);
-		// button.setOnClickListener(this);
+		setContentView(R.layout.main);
+
+		final Button button = (Button) findViewById(R.id.incluirNuevaNota);
+		button.setOnClickListener(this);
 	}
 
 	public void onClick(final View v) {
-		final TextView textView = (TextView) findViewById(R.id.insertarNota);
-		textView.setText(R.string.notaInsertada);
+		final EditText campoNuevaNota = (EditText) findViewById(R.id.nuevaNota);
+		final String textoNota = campoNuevaNota.getText().toString();
+		notas.add(textoNota);
+		Log.e(NOTAS_TAG, textoNota);
 	}
 }
