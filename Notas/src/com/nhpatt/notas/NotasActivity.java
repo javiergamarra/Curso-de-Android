5a6b95f2cbf6cda63441f5aa0a495e6c3d6a0ad1
Javier Gamarra <nhpatt@gmail.com>
2011-10-09 14:08:15 +0200

    Ejercicio 9: listView con las notas insertadas

--- Notas/src/com/nhpatt/notas/NotasActivity.java
+++ Notas/src/com/nhpatt/notas/NotasActivity.java
@@ -3,23 +3,27 @@ package com.nhpatt.notas;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class NotasActivity extends Activity implements OnClickListener {
+public class NotasActivity extends ListActivity implements OnClickListener {
 
-	private static final String NOTAS_TAG = "NOTAS";
 	private final List<String> notas = new ArrayList<String>();
+	private ArrayAdapter<String> arrayAdapter;
 
 	@Override
 	public void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+
+		arrayAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, notas);
+		setListAdapter(arrayAdapter);
 
 		final Button button = (Button) findViewById(R.id.incluirNuevaNota);
 		button.setOnClickListener(this);
@@ -27,8 +31,8 @@ public class NotasActivity extends Activity implements OnClickListener {
 
 	public void onClick(final View v) {
 		final EditText campoNuevaNota = (EditText) findViewById(R.id.nuevaNota);
-		final String textoNota = campoNuevaNota.getText().toString();
-		notas.add(textoNota);
-		Log.e(NOTAS_TAG, textoNota);
+		notas.add(campoNuevaNota.getText().toString());
+		arrayAdapter.notifyDataSetChanged();
+		campoNuevaNota.setText("");
 	}
 }
