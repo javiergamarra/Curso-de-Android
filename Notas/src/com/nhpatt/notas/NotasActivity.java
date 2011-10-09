273694d5c9619524cdbed7c5b576e14a0a402399
Javier Gamarra <nhpatt@gmail.com>
2011-10-09 13:23:47 +0200

    Ejercicio 4: utilizar interfaces definidas en XML

--- Notas/src/com/nhpatt/notas/NotasActivity.java
+++ Notas/src/com/nhpatt/notas/NotasActivity.java
@@ -2,31 +2,11 @@ package com.nhpatt.notas;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.ViewGroup.LayoutParams;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class NotasActivity extends Activity {
 	@Override
 	public void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		final Button button = new Button(this);
-		button.setText(R.string.salir);
-
-		final TextView textView = new TextView(this);
-		textView.setText(R.string.insertarNota);
-
-		final LinearLayout linearLayout = new LinearLayout(this);
-		linearLayout.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT,
-				LayoutParams.WRAP_CONTENT));
-		linearLayout.setOrientation(LinearLayout.VERTICAL);
-		linearLayout.addView(textView);
-		linearLayout.addView(button);
-		setContentView(linearLayout);
-
-		// setContentView(R.layout.main);
-
+		setContentView(R.layout.main);
 	}
 }
